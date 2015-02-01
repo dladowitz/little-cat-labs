d3b3367497a54bbe2929c0e315ede5a939b3f11d
David Ladowitz <david@ladowitz.com>
2015-01-31 23:41:20 -0800

    testing out weight api

---
+++ app/controllers/api/v1/weights_controller.rb
@@ -0,0 +1,26 @@
+module Api
+  module V1
+    class WeightsController < ApplicationController
+
+      # TODO figure out what this does. Is it a security risk.
+      skip_before_filter :verify_authenticity_token
+
+      respond_to :json
+
+      def create
+        # Need to create some kind of authentication
+        # Not sure why cat_id is inside of weight hash in params
+
+        if params[:scale_id] == "001"
+          weight = Weight.create(amount: params[:weight_amount], cat_id: params[:cat_id])
+          respond_with :api, :v1, weight
+        end
+      end
+    end
+  end
+end
+
+
+
+# Test post command
+# curl -H "Content-Type: application/json" -d '{"weight_amount":"13.5", "cat_id":"30", "scale_id": "001"}' http://localhost:3000/api/v1/weights
--- config/routes.rb
+++ config/routes.rb
@@ -24,6 +24,12 @@ Rails.application.routes.draw do
     end
   end
 
+  namespace :api do
+    namespace :v1 do
+      resources :weights
+    end
+  end
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
